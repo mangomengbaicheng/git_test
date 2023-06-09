b0f633956f7ba0b157e5df0f8a18e7d5d06dc66b
mengliang <274107336@qq.com>
2023-06-09 13:35:08 +0800

    first commit

---
+++ src/main/java/com/mango/GitTestMain.java
@@ -0,0 +1,11 @@
+package com.mango;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestMain {
+    public static void main(String[] args) {
+        System.out.println("my git");
+    }
+}
