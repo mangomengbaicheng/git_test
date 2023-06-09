edf122c8db09a5e1bd15663a8155a7ca65fbf25e
mengliang <274107336@qq.com>
2023-06-09 13:44:39 +0800

    hot_fix commit

--- src/main/java/com/mango/GitTestMain.java
+++ src/main/java/com/mango/GitTestMain.java
@@ -8,5 +8,6 @@ public class GitTestMain {
     public static void main(String[] args) {
         System.out.println("my git");
         System.out.println("my git1");
+        System.out.println("my git1 hot_fix");
     }
 }
